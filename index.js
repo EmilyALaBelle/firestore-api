4fa9bb520294331d5aebf69766484cccc9cba94e
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-18 11:57:39 -0400

    first

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createCoffee } from "./src/coffees.js"
+import { createCoffee, getAllCoffees } from "./src/coffees.js"
 
 const app = express()
 app.use(express.json())
 
 app.post("/coffees", createCoffee)
+app.get('/coffees', getAllCoffees)
 
 
 app.listen(3030, () => console.log("listening on http://localhost:3030..."))
